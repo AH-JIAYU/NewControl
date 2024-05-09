97b5611f69d99a193ed2fe8f15f8629301e1e65f
llg <3102093662@qq.com>
2024-05-09 15:35:33 +0800

    菜单接口

--- src/api/modules/app.ts
+++ src/api/modules/app.ts
@@ -1,10 +1,12 @@
 import api from '../index'
 
 export default {
-  // 后端获取路由数据
+  // 后端获取路由数据 mock
   routeList: () => api.get('app/route/list', {
     baseURL: '/mock/',
   }),
+  // 后端接口路由
+  // routeList: () => api.get('menu/getMenuList'),
 
   // 基于文件系统路由模式下，后端获取导航菜单数据
   menuList: () => api.get('app/menu/list', {
